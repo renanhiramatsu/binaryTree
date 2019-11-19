9957ed5a784f6964d191e3a5c4bc6ce399e1f91b
amberlow <56101050+amberlow@users.noreply.github.com>
2019-11-18 22:02:24 -0800

    Add files via upload

--- person.h
+++ person.h
@@ -12,8 +12,8 @@ private:
 public:
 
 	//constructors
-	Person() {};
-	Person(std::string, std::string, int, int, int);
+	Person();
+	Person(std::string, std::string);
 
 	//setters
 	void setName(std::string fn, std::string ln);
@@ -35,12 +35,29 @@ Person::Person() {
 }
 
 
-Person::Person(std::string fn, std::string ln, int bm, int bd, int by) {
-	fName = fn;
-	lName = ln;
-	bMonth = bm;
-	bDate = bd;
-	bYear = by;
+Person::Person(std::string name, std::string bday) {
+	std::cout << bday << std::endl;
+	int pos1;
+	int l = name.length();
+	pos1 = name.find(" ", 0);
+	fName = name.substr(0, pos1);
+	lName = name.substr(pos1 + 1, l - pos1);
+
+	int pos2, pos3;
+	int length2 = bday.length();
+	pos2 = bday.find("-", 0);
+	bYear = stoi(bday.substr(0, pos2));
+	pos2++;
+	pos3 = bday.find("-", pos2);
+	bMonth = stoi(bday.substr(pos2, length2 - pos3));
+	bDate = stoi(bday.substr(pos3 + 1, length2));
+	std::cout << "Bdate: " << bDate << std::endl;
+	std::cout << "BMONTH: " << bMonth << std::endl;
+	std::cout << "BYear: " << bYear << std::endl;
+
+	std::cout << "last name: " << lName << std::endl;
+	std::cout << "first name: " << fName << std::endl;
+	std::cout << std::endl;
 }
 
 void Person::setName(std::string fn, std::string ln) {
